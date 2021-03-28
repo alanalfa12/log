e7959c11879eb82e9631014982e52c62acf5dd72
alanalfa12 <alanalfrz@gmail.com>
2021-03-29 03:21:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ global.logger = require('./src/miscellaneous/logger')
 global.webhook = require('./src/miscellaneous/webhooklogger')
 global.cluster = require('cluster')
 require('./src/miscellaneous/logger')
+require('/uptime.js')
 require('dotenv').config()
 if (cluster.isMaster) {
   console.log('Master node init')
